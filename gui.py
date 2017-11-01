3f9ff26bc71f41e0be1fa1637a8a83c0a64fc099
Eyal Nesher <eyal.nesher@gmail.com>
2017-11-02 01:00:42 +0200

    The final gui

--- gui.py
+++ gui.py
@@ -3,11 +3,17 @@
 from Tkinter import *
 from multiprocessing import Process
 import active_DGW_determinate as dgw
-import sys
-from scapy.all import *
+from time import time
 
 
 def time_parsing(inp_time):
+    """
+    turning time from time units format to seconds
+    :param inp_time: The time in time units format
+    :type inp_time: str
+    :return: The time in seconds
+    :rtype: Union[float, int]
+    """
     if inp_time == "":
         return float("inf")
     time_units = inp_time.split(":")
@@ -20,30 +26,65 @@ def time_parsing(inp_time):
         raise ValueError("Must be at most 3 time units ({} given)".format(len(time_units)))
 
 
-def start(run_time):
+def start():
     """
-    The actions that happend when the start button is clicked
-    :return:
+    Calling to the actual code.
+    :return: None
+    :rtype: None
     """
-    
-    running_time = time_parsing(run_time)
-    print "starting sniffing"
-    dgw.main(running_time)
+    dgw.main()
 
 
 def starting_process():
+    """
+    The actions that happen when the start button is clicked
+    :return: None
+    :rtype: None
+    """
     global time_entry
     global log
 
-    pstart = Process(target=start, args=(time_entry.get(),))
+    # Clear the log
+    clear_log = open("result.log", "w")
+    clear_log.close()
+    try:
+        log.delete(0, END)
+    except:
+        pass
+
+    running_time = time_parsing(time_entry.get())   # The running time
+
+    pstart = Process(target=start)
+
+    starting_time = time()   # The starting time
     pstart.start()
+
+    # Time checking
+    while True:
+        current_time = time()
+        if (current_time - starting_time > running_time) or (len(open("result.log", "r").read().split("\n")) > 1):
+            pstart.terminate()
+            break
+        elif not pstart.is_alive():
+            break
+
     result = open("result.log", "r").read()
-    log.insert(INSERT, result)
+
+    # Detales checking
+    if len(result) == 0:
+        open("result.log", "w").write("The Default Gateway could not be found")
+    elif len(result.split("\n")) == 1:
+        open("result.log", "w").write(result + "The IP address of the Default Gateway could not be found")
+
+    updated_result = open("result.log", "r").read()
+
+    log.insert(INSERT, updated_result)
+
 
 
 def main():
     """
-
+    The GUI of the DGW finder.
     :return:
     """
     global log
@@ -54,7 +95,7 @@ def main():
     time_frame = Frame(root)
     time_frame.pack(side=TOP)
 
-    time_label = Label(time_frame, text="time for program to run for:")
+    time_label = Label(time_frame, text="The program duration time:")
     time_label.pack(side=LEFT)
 
     global time_entry
